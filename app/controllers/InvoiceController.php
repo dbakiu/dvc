0fda6c7ea00877efc73ace98cb0bee1d53c6153d
dritonb <dbakiu@gmail.com>
2014-09-22 17:32:10 +0200

    Added the laravel-dompdf library. Added the header.

--- app/controllers/InvoiceController.php
+++ app/controllers/InvoiceController.php
@@ -3,18 +3,19 @@
 class InvoiceController extends BaseController {
 
 
-    public function index()
-    {
+    public function index(){
         return View::make('invoice.index');
     }
 
     public function create(){
-        return View::make('invoice.add');
+       return View::make('invoice.add');
     }
 
     public function store(){
         // TODO
         // Add storing and pdf output
+        #$pdf = PDF::loadView('invoice.pdf');
+        #return $pdf->download('invoice.pdf');
     }
 
     public function show($id){
