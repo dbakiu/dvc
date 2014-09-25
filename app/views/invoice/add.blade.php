8b9921857becaa0804353219fbdb2703d1492a5c
dritonb <dbakiu@gmail.com>
2014-09-25 02:06:35 +0200

    Added invoice element manipulation - adding and removing.

--- app/views/invoice/add.blade.php
+++ app/views/invoice/add.blade.php
@@ -5,24 +5,19 @@
 <div class="invoice_form_wrapper">
     <p class="center_form_title">New invoice</p>
     {{ Form::open(['route' => 'invoice.store']) }}
+    {{ Form::text('bill_to', null, ['placeholder' => 'Bill to'] ) }}
     {{ Form::text('date', null, ['placeholder' => 'Invoice date'] ) }}
 
-    {{ Form::text('bill_to', null, ['placeholder' => 'Bill to'] ) }}
-
-    {{ Form::text('admin_name', null, ['placeholder' => '#Name'] ) }}
-
-    {{ Form::text('vat_number', null, ['placeholder' => 'Vat number'] ) }}
+    {{ Form::label('employee_fk', 'Employee')  }}
+    {{ Form::select('employee_fk', $employeesList, null,  ['id' => 'employee_fk']  ) }}
     <br />
     <hr />
-    {{ Form::close() }}
     {{ Form::text('valeted_date', null, ['id' => 'valeted_date', 'placeholder' => 'Date']) }}
     {{ Form::text('quantity', null, ['id' => 'quantity', 'placeholder' => 'Quantity']) }}
 
     {{ Form::label('vehicle_fk', 'Vehicle type')  }}
     {{ Form::select('vehicle_fk', $vehiclesList, null, ['id' => 'vehicle_fk']  ) }}
 
-    {{ Form::label('employee_fk', 'Employee')  }}
-    {{ Form::select('employee_fk', $employeesList, null,  ['id' => 'employee_fk']  ) }}
 
     {{ Form::select('vehicles_pricelist', $vehiclesPricelist, null, ['id' => 'vehicles_pricelist', 'class' => 'hidden']  ) }}
 
@@ -43,7 +38,10 @@
             </tbody>
         </table>
     </div>
+      {{ Form::text('elements_list', null, ['id' => 'elements_list', 'class' => 'hidden']) }}
+
     </div>
+    {{ Form::close() }}
 </div>
 
 @stop
--- public/js/invoice-script.js
+++ public/js/invoice-script.js
@@ -1,27 +1,26 @@
 $(document).ready(function(){
+
     console.log("file loaded");
 
     // Events
 
     $("#add_element").click(addElement);
-    $(document).on('click', "#delete_element", function(){
+    $(document).on('click', ".delete_element", function(){
         deleteElement($(this));
     });
 
 
     // Functions
 
+    var counter = 1;
     function addElement(){
         console.log('adding element');
+
         var date = $('#valeted_date').val();
         var quantity = $('#quantity').val();
 
         var vehicle_fk = $('#vehicle_fk').val();
         var vehicle_type  = $('#vehicle_fk option:selected').text();
-
-        var employee_fk = $('#employee_fk').val();
-        var employee_name = $('#employee_fk option:selected').text();
-
 
         var unit_price = getVehiclePrice();
         if(quantity > 0) {
@@ -35,21 +34,43 @@ $(document).ready(function(){
                                     + '<td>' + quantity  + ' cars valeted on ' + date + ' ' + vehicle_type + '</td>'
                                     + '<td>£' + unit_price + '</td>'
                                     + '<td>£' + line_total + '</td>'
-                                    + '<td>' + '<input type="button" value = "X" id="delete_element">' + '</td>'
+                                    + '<td>' + '<input type="button" id="' + counter + '" class="delete_element" value="X">' + '</td>'
                                     + '</tr>');
 
-        //var element_obj = {};
+        var invoice_element = {
+                                'index' : counter,
+                                'quantity' : quantity,
+                                'vehicle_fk' : vehicle_fk,
+                                'date' : date
+                              };
 
-        //element_obj[employee]
+        storeInvoiceElement(invoice_element);
+        counter++;
     }
-
 
 
     function deleteElement(thisObj){
         console.log('removing element');
-        $(thisObj).closest("tr").remove();
-    }
 
+        // Remove the HTML element.
+        $(thisObj).closest("tr").remove();
+
+        var targetElement = thisObj.attr('id');
+        var currentList = $.parseJSON($('#elements_list').val());
+        var arrayLength = currentList.length;
+
+        for (var i = 0; i < arrayLength; i++) {
+            // If it's an actual object that contains the "index" element, proceed.
+            if(currentList[i]['index']) {
+                if (currentList[i]['index'] == targetElement) {
+                    currentList.splice(i, 1);
+                }
+            }
+        }
+
+        $('#elements_list').val(JSON.stringify(currentList));
+        console.log(currentList);
+    }
 
 
     function getVehiclePrice(){
@@ -63,6 +84,38 @@ $(document).ready(function(){
         return price;
     }
 
+
+    function storeInvoiceElement(invoice_element){
+        console.log('storing element..');
+
+        // Add a trailing comma if there are more than one element. Remove trailing comma after adding, place brackets. Rinse, repeat.
+        var jsonElement = JSON.stringify(invoice_element);
+
+        var currentList = $('#elements_list').val();
+
+        // If the array is JSONified, remove the brackets.
+        if(currentList.substr(0, 1) == '['){
+            // If the array is not empty, delete the brackets, add a comma at the end.
+            if(currentList.substr(1, 1) != ']') {
+                currentList = currentList.substr(1);
+                currentList = currentList.slice(0, -1);
+                currentList = currentList + ', ';
+            }
+            // If the array is empty, clear the field entirely.
+            else{
+                $('#elements_list').val('');
+                currentList = $('#elements_list').val();
+            }
+        }
+
+        $('#elements_list').val( currentList + jsonElement + ',');
+
+        var newList = $('#elements_list').val();
+        newList = newList.slice(0, -1);
+
+        $('#elements_list').val('[' + newList + ']');
+
+    }
 
 
 // Client-side check if the quantity is a numeric value.
