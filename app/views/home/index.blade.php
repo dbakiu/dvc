63a29b29f8aa419e72b275b79d3fe5159ffb04b0
dritonb <dbakiu@gmail.com>
2014-10-09 03:31:38 +0200

    Changed the appearance of the homepage 'menu' to a vertical, fixed-width list.

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -10,30 +10,37 @@
 
     @endif
 
-    <ul class="home_page_menu">
+    <ul class="nav nav-pills nav-stacked span2">
+
         <a href="{{ route('invoice.create') }}"><li>
-            <button class="btn btn-info btn-lg" type="button">
+            <button class="btn btn-info btn-lg btn-block" type="button">
                 New invoice
             </button>
         </li></a>
 
         <a href="{{ route('vehicle.index') }}"><li>
-            <button class="btn btn-info btn-lg" type="button">
-                Price list and vehicles
+            <button class="btn btn-info btn-lg btn-block" type="button">
+               Pricelist
             </button>
         </li></a>
+
         <a href="{{ route('employee.index') }}"><li>
-            <button class="btn btn-info btn-lg" type="button">
+            <button class="btn btn-info btn-lg btn-block" type="button">
                  Employee management
             </button>
         </li></a>
+        <a href="{{ route('wages') }}"><li>
+            <button class="btn btn-info btn-lg btn-block" type="button">
+                Wages
+            </button>
+        </li></a>
         <a href="{{ route('expense.index') }}"><li>
-            <button class="btn btn-info btn-lg" type="button">
+            <button class="btn btn-info btn-lg btn-block" type="button">
                 Expenses
             </button>
         </li></a>
         <a href="{{ route('balance.index') }}"><li>
-            <button class="btn btn-info btn-lg" type="button">
+            <button class="btn btn-info btn-lg btn-block" type="button">
                 Balance
             </button>
         </li></a>
